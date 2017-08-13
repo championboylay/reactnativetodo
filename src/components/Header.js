844bb8bafefb42bf2f79b51b834381ca5c06084e
Aung Myat Hein <AungMyatHein@startinpoint.com>
2017-08-13 08:25:19 +0630

    Added toggle complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import { TextInput, Text, View } from "react-native";
+import { TextInput, Text, View, TouchableOpacity } from "react-native";
 
 export default class Main extends Component {
   render() {
     return (
       <View style={styles.header}>
-        <TextInput value={this.props.value} onChangeText={this.props.onChange} onSubmitEditing={this.props.onAddItem} style={styles.task} placeholder="What needs to do?" />
+        <TouchableOpacity onPress={this.props.onToggleAllComplete}>
+          <Text style={styles.toggleIcon}>
+            {String.fromCharCode(10003)}
+          </Text>
+        </TouchableOpacity>
+        <TextInput
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          style={styles.task}
+          placeholder="What needs to do?"
+        />
       </View>
     );
   }
@@ -14,10 +25,15 @@ export default class Main extends Component {
 const styles = {
   header: {
     flexDirection: "row",
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   task: {
     flex: 1,
     fontSize: 16
+  },
+  toggleIcon: {
+    fontSize: 30
   }
 };
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -6,26 +6,47 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      allComplete: false,
       value: "",
       items: []
     };
   }
 
+  handleToggleAllComplete() {
+    const complete = !this.state.allComplete;
+    const newItems = this.state.items.map(item => {
+      return { ...item, complete };
+    });
+
+    console.log("AFTER", newItems);
+    this.setState({
+      items: newItems,
+      allComplete: complete
+    });
+  }
   handleAddingTask() {
+    console.log(this.state.value);
     if (!this.state.value) return;
-    const items = this.state.items;
-    const newItems = [
-      this.state.items,
+    const items = [
+      ...this.state.items,
       {
-        key: new Date().now,
+        key: new Date(),
         value: this.state.value,
         complete: false
       }
     ];
+
     this.setState({
-      items: newItems,
+      items,
       value: ""
     });
+    console.log(this.state);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.items > prevState.items) {
+      console.log("DidUpdate", this.state.items);
+    }
   }
   render() {
     return (
@@ -34,6 +55,7 @@ export default class Main extends Component {
           value={this.state.value}
           onAddItem={this.handleAddingTask.bind(this)}
           onChange={value => this.setState({ value })}
+          onToggleAllComplete={this.handleToggleAllComplete.bind(this)}
         />
       </View>
     );
