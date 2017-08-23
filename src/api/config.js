d258712b3f7ff8e21fe217ef0db21fb6ad3638eb
SIPDOMAIN\AungMyatHein <aungmyathein@startinpoint.com>
2017-08-23 09:50:18 +0630

    modified firebase config

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
-export const firebaseConfig = {
+import * as firebase from "firebase";
+const config = {
   apiKey: "AIzaSyAA_3l00XRrVsqazERokyn0zHC-XCYIAuY",
   authDomain: "reactnativetodo-36f23.firebaseapp.com",
   databaseURL: "https://reactnativetodo-36f23.firebaseio.com",
@@ -6,3 +7,6 @@ export const firebaseConfig = {
   storageBucket: "",
   messagingSenderId: "849336920424"
 };
+
+firebase.initializeApp(config);
+export default firebase;
