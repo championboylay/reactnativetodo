d4132b5f1b576844fc306f991ae48f093072e080
SIPDOMAIN\AungMyatHein <aungmyathein@startinpoint.com>
2017-08-29 15:30:24 +0630

    Fixed saga combination problem and can navigate via redux saga

--- src/actions/Navigation.action.js
+++
@@ -1,6 +0,0 @@
-import { NavigationActions } from "react-navigation";
-
-const navigateTo = routeName => () => NavigationActions.navigate({ routeName });
-
-export const navigateToMainScreen = navigateTo("MainScreen");
-export const navigateToTaskFormScreen = navigateTo("TaskFormScreen");
